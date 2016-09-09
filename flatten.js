72795e5a21886ddccbcbab19d94071f33512895b
Sean Kendrick <seankendrick12@gmail.com>
2016-09-08 22:53:55 -0400

    js flatten added

---
+++ flatten.js
@@ -0,0 +1,20 @@
+function flatten(arr){
+  var newarr = [];
+  for(var i = 0; i<arr.length; i++){
+    if(Array.isArray(arr[i])){
+      if(arr[i].length>0){
+        var rep = flatten(arr[i]);
+        for(var j = 0; j<rep.length; j++){
+          newarr.push(rep[j]);
+        }
+      }
+    }
+    else{
+      newarr.push(arr[i]);
+    }
+  }
+  return newarr;
+}
+
+
+console.log(flatten([4,3,6,[],7,8,[4,5,6,[9,8,[6,5,4,3,[1,2,3,[5,4]]]],6],6,7,5,4]));
